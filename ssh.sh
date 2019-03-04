bc6701bccf9cad372c960bf631ea58d509652ebf
Jianchao Wang <jcwang1984@gmail.com>
2019-03-04 10:15:15 +0000

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,15 @@
-#! /bin/bash
 file="list.txt"
+read -p "please enter id and key (-u ****): " username
+read -p "please enter new jenkins pack name: " jenkinspack
+
 cat $file | while read line
-do 
-  echo "ec2@$line"
+do
+  echo "Working on $line"
+  ssh root@$line " cd /usr/lib/jenkins/ && \
+                   mv jenkins.war jenkins.war.old &>> output && \
+                   curl -u$username -o jenkins.war 'Http://$jenkinspack' &>> output && \
+                   service jenkins restart &>> output && \
+                   exit"
+  echo "Completed work on $line"
+
 done
-  
